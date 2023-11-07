466c56c4e0d5508187f93cb7b2179331f42d3366
MartaBelmonte <martabelmonts5@gmail.com>
2023-11-07 18:25:07 +0100

    contact-icons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,18 +3,29 @@ import '../../styles/App.css';
 import MartaImage from '../../Images/MartaImage.png';
 
 function Contact() {
+  const linkedinURL = 'https://www.linkedin.com/in/marta-belmonte'; 
+  const githubURL = 'https://github.com/MartaBelmonte'; 
+
   return (
     <section className="Contact">
-        <div className="PortraitContainer">
-          <img className="Portrait" src={MartaImage} alt="Marta's Portrait" />
-        </div>
-        <div className="ContactInfo">
-          <h2>Contáctame</h2>
-          <p>Linkedin</p>
-          <p>Github</p>
-          <p>Correo electrónico: marta@martabelmonte.es</p>
-        </div>
-      </section>
+      <div className="PortraitContainer">
+        <img className="Portrait" src={MartaImage} alt="Marta's Portrait" />
+      </div>
+      <div className="ContactInfo">
+        <h2>Contáctame</h2>
+        <p>
+          <a href={linkedinURL} target="_blank" rel="noopener noreferrer">
+            <i className="fa-brands fa-linkedin-in fa-beat"></i> LinkedIn
+          </a>
+        </p>
+        <p>
+          <a href={githubURL} target="_blank" rel="noopener noreferrer">
+            <i className="fa-brands fa-github fa-beat"></i> GitHub
+          </a>
+        </p>
+        <p>marta@martabelmonte.es</p>
+      </div>
+    </section>
   );
 }
 
