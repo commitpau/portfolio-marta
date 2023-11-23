ddc6e873c972148a34aa0d0a72931b11c4ab1080
MartaBelmonte <martabelmonts5@gmail.com>
2023-11-23 11:56:43 +0100

    header

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import '../styles/App.css';
 import ProjectList from '../components/ProjectList/ProjectList';
 import Contact from '../components/Contact/Contact';
 import AboutMe from '../components/AboutMe/AboutMe';
+import MartaImage from '../Images/MartaImage.png';
+
+function Navigation() {
+  return (
+    <nav className="Navigation">
+      <ul>
+        <li><Link to="/">About Me</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>Marta Belmonte</h1>
-          <h4>Full-Stack Developer</h4>
+          <div className="header-left">
+            <h1>Marta Belmonte</h1>
+            <h4>Full-Stack Developer</h4>
+          </div>
+          <Navigation />
         </header>
         <Routes>
-          <Route path="/" element={<AboutMe />} />
-        </Routes>
-        <Routes>
-          <Route path="/" element={<ProjectList />} />
-        </Routes>
-        <Routes>
-          <Route path="/" element={<Contact />} />
-        </Routes>
+            <Route path="/" element={<React.Fragment><AboutMe /><ProjectList /><Contact /></React.Fragment>} />
+            <Route path="/projects" element={<ProjectList />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
       </div>
     </Router>
   );
 }
 
 export default App;
+
 
 
 
